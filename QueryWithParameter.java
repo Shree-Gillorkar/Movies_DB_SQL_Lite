c086b119d759097046144a11a8b7be7083953745
Shree Gillorkar <70030205+Shree-Gillorkar@users.noreply.github.com>
2021-09-23 22:19:59 +0530

    Update QueryWithParameter.java

--- QueryWithParameter.java
+++ QueryWithParameter.java
@@ -23,7 +23,7 @@ import java.sql.Statement;
     }
 	
 	public void Parameter(){
-               String sql = "SELECT * from Movies where Lead_Actress like '%Kareena Kapoor%';";
+               String sql = "SELECT * from Movies where Lead_Actress like '%Shraddha Kapoor%';";
         
         try (Connection conn = this.connect();
               PreparedStatement pstmt  = conn.prepareStatement(sql)){
